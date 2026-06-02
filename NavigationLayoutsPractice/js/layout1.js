5693377e30eeb9e56bb5ca01722b8f8084d7dcbe
Shatikka McKnight <dev.tikkaknight.wgs@gmail.com>
2026-06-02 17:03:19 -0400

    Add the scss updates

---
+++ NavigationLayoutsPractice/js/layout1.js
@@ -0,0 +1,5 @@
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.navLink');
+hamburger.addEventListener('click', () =>{
+    navLink.classList.toggle('active');
+});
